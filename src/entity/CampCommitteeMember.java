801b520bdb7192fa58c84c52ef71afb63fff1bc0
jonddd <jond0001@e.ntu.edu.sg>
2023-11-01 14:23:01 +0800

    camp committee member skeleton added

---
+++ src/entity/CampCommitteeMember.java
@@ -0,0 +1,46 @@
+package entity;
+
+import control.LoginSystem;
+import util.Log;
+
+/**
+ * <p>
+ * This is an entity class to represent a camp committee member
+ * </p>
+ * 
+ * @author Jon Daniel Acu Kang
+ * @version 1.0
+ * @since 1-11-2023
+ */
+public class CampCommitteeMember implements Student{
+    private int campId;
+    private int points;
+
+    public void viewCampDetails (int campId) {
+
+    }
+
+    public void makeSuggestion () {
+
+    }
+
+    public void replyEnquiries () {
+
+    }
+
+    public void viewSuggestions () {
+
+    }
+
+    public void editSuggestion () {
+
+    }
+    
+    public void deleteSuggestion () {
+
+    }
+
+    public void generateCampReport (ReportOptions reportOptions, ReportFilter reportFilter) {
+        
+    }
+}
