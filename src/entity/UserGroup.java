3fe0a333029364916808c69085af3bd5f5df583a
tersimdev <tersimdev@gmail.com>
2023-11-05 19:17:36 +0800

    added user group class

---
+++ src/entity/UserGroup.java
@@ -0,0 +1,68 @@
+package entity;
+
+import util.Log;
+import util.DataStore.SerializeToCSV;
+
+/**
+ * <p>
+ * This is a class to represent user group of a camp
+ * It is either faculty, in which it has a faculty enum to specify,
+ * or it is the whole NTU
+ * </p>
+ * 
+ * @author Sim Yi Wan Terence
+ * @version 1.0
+ * @since 1-11-2023
+ */
+public class UserGroup implements SerializeToCSV {
+    private boolean wholeNTU;
+    private Faculty faculty;
+
+    public UserGroup() {
+        faculty = null;
+    }
+
+    public boolean isWholeNTU() {
+        return wholeNTU;
+    }
+
+    public Faculty getFaculty() {
+        return faculty;
+    }
+
+    public UserGroup setWholeNTU() {
+        wholeNTU = true;
+        faculty = null;
+        return this;
+    }
+
+    public UserGroup setFaculty(Faculty faculty) {
+        this.faculty = faculty;
+        wholeNTU = false;
+        return this;
+    }
+
+    @Override
+    public String toCSVLine() {
+        String ret = "";
+        ret += (wholeNTU ? "NTU" : "FACULTY") + ","
+                + faculty.toString();
+        return ret;
+    }
+
+    @Override
+    public void fromCSVLine(String csvLine) {
+        String[] split = csvLine.split(",");
+        if (split.length != 2) {
+            Log.error("csvLine is invalid");
+        } else {
+            wholeNTU = split[0].trim().equals("NTU");
+            try {
+                faculty = Faculty.valueOf(split[1]);
+            } catch (IllegalArgumentException e) {
+                Log.error("invalid faculty, doesnt correspond to enum: " + split[1]);
+                faculty = Faculty.NULL; // give it null value
+            }
+        }
+    }
+}
