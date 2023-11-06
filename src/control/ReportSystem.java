e8f899378c720b5e5bad0e53779af0124f460841
ryan <ryanlimjunrong@gmail.com>
2023-11-07 01:30:04 +0800

    added generateReport method

--- src/control/ReportSystem.java
+++ src/control/ReportSystem.java
@@ -1,13 +1,17 @@
 package control;
 
+import entity.CampReportOptions;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  * <p>
  * A singleton class to generate reports for staff and committee memhers 
  * </p>
  * 
- * @author
+ * @author Lim Jun Rong Ryan
  * @version 1.0
- * @since 1-11-2023
+ * @since 7-11-2023
  */
 public class ReportSystem {
     private static ReportSystem instance = null;
@@ -19,5 +23,18 @@ public class ReportSystem {
         if (instance == null)
             instance = new ReportSystem();
         return instance;
+    }
+
+    public void generateReport(CampReportOptions reportOptions, String reportContent){
+        String fileName = reportOptions.getFileName() + reportOptions.getFileType();
+        try{
+            FileWriter fileWriter = new FileWriter(fileName);
+            fileWriter.write(reportContent);
+            fileWriter.close();
+            System.out.println("Report Generated: " + fileName);
+        } catch(IOException exception){
+            exception.printStackTrace();
+            System.err.println("Error generating the report. Please try again");
+        }
     }
 }
