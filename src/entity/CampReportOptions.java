331487a6a607ecd2f02a6170df0d745eb9c14822
ryan <ryanlimjunrong@gmail.com>
2023-11-06 23:53:36 +0800

    added get/set methods

--- src/entity/CampReportOptions.java
+++ src/entity/CampReportOptions.java
@@ -2,18 +2,39 @@ package entity;
 
 /**
  * <p>
- * Class to group together camp opitions for report genration
+ * Class to group together camp opitions for report generation
  * </p>
  * 
- * @author
+ * @author Lim Jun Rong Ryan
  * @version 1.0
- * @since 5-11-2023
+ * @since 6-11-2023
  */
 public class CampReportOptions {
     private int campId;
     private String fileType = ".txt";
     private String fileName = "output";
-    
-    
-    //TODO
+
+    public int getCampId(){
+        return campId;
+    }
+
+    public void setCampId(int campId){
+        this.campId = campId;
+    }
+
+    public String getFileType(){
+        return fileType;
+    }
+
+    public void setFileType(String fileType){
+        this.fileType = fileType;
+    }
+
+    public String getFileName(){
+        return fileName;
+    }
+
+    public void setFilename(String fileName){
+        this.fileName = fileName;
+    }
 }
