d18a3bba687d2c32ef1714db260e1e778e73b1c1
ryan <ryanlimjunrong@gmail.com>
2023-11-18 15:43:07 +0800

    Updated ReportSystem

--- src/control/ReportSystem.java
+++ src/control/ReportSystem.java
@@ -1,18 +1,20 @@
 package control;
 
+import entity.Camp;
 import entity.CampReportFilter;
 import entity.CampReportOptions;
+import entity.Student;
+import entity.User;
+
 import java.io.FileWriter;
 import java.io.IOException;
 
 /**
- * <p>
- * A singleton class to generate reports for staff and committee memhers 
- * </p>
+ * A singleton class to generate reports for staff and committee members.
  * 
  * @author Lim Jun Rong Ryan
  * @version 1.1
- * @since 7-11-2023
+ * @since 18-11-2023
  */
 public class ReportSystem {
     private static ReportSystem instance = null;
@@ -26,15 +28,43 @@ public class ReportSystem {
         return instance;
     }
 
-    public void generateReport(CampReportOptions reportOptions, String reportContent){
-        String fileName = reportOptions.getFileName() + reportOptions.getFileType();
-        try{
+    public static class ReportGenerator {
+
+        public static String generateCampReport(User user, Camp camp) {
+            if (camp == null || camp.getCampInfo() == null) {
+                return "Invalid camp or camp information is missing.";
+            }
+
+            StringBuilder reportContent = new StringBuilder();
+        
+            reportContent.append("Camp Name: ").append(camp.getCampId()).append("\n");
+            reportContent.append("Dates: ").append(camp.getCampInfo().getDates()).append("\n");
+        
+            reportContent.append("\nCamp Attendees:\n");
+            for (Student attendee : camp.getAttendees()) {
+                reportContent.append("- ").append(attendee.getDisplayName()).append("\n");
+            }
+        
+            reportContent.append("\nCamp Committee Members:\n");
+            for (Student committeeMember : camp.getCommitteeMembers()) {
+                reportContent.append("- ").append(committeeMember.getDisplayName()).append("\n");
+            }
+        
+            return reportContent.toString();
+        }
+    }
+
+    public void generateReport(CampReportOptions reportOptions, User user, Camp camp) {
+        String reportContent = ReportGenerator.generateCampReport(user, camp);
+
+        String fileName = reportOptions.getFilePath() + reportOptions.getFileName() + reportOptions.getFileType();
+        try {
             FileWriter fileWriter = new FileWriter(fileName);
             String filteredReportContent = applyFilter(reportContent, reportOptions.getFilter());
             fileWriter.write(filteredReportContent);
             fileWriter.close();
             System.out.println("Report Generated: " + fileName);
-        } catch(IOException exception){
+        } catch (IOException exception) {
             exception.printStackTrace();
             System.err.println("Error generating the report. Please try again");
         }
@@ -47,20 +77,19 @@ public class ReportSystem {
         for(String line : lines){
             switch(filter){
                 case ATTENDEE:
-                if(line.contains("ATTENDEE")){
-                    filteredContent.append(line).append("\n");
-                }
-            break;
+                    if(line.contains("ATTENDEE")){
+                        filteredContent.append(line).append("\n");
+                    }
+                    break;
                 case CAMP_COMMITTEE:
-                if(line.contains("CAMP_COMMITTEE")){
+                    if(line.contains("CAMP_COMMITTEE")){
+                        filteredContent.append(line).append("\n");
+                    }
+                    break;
+                default:
                     filteredContent.append(line).append("\n");
-                }
-            break;
-        default:
-                filteredContent.append(line).append("\n");
             }
         }
         return filteredContent.toString();
     }
-
 }
--- src/entity/CampReportOptions.java
+++ src/entity/CampReportOptions.java
@@ -7,13 +7,14 @@ package entity;
  * 
  * @author Lim Jun Rong Ryan
  * @version 1.1
- * @since 7-11-2023
+ * @since 18-11-2023
  */
 public class CampReportOptions {
     private int campId;
     private String fileType = ".txt";
     private String fileName = "output";
     private CampReportFilter filter;
+    private String filePath = "./"; // Set to current directory
 
     public int getCampId(){
         return campId;
@@ -45,6 +46,14 @@ public class CampReportOptions {
 
     public void setFilter(CampReportFilter filter){
         this.filter = filter;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
     }
 
 }
