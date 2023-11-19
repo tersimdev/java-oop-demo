f88227ac0d91b1433b52b489ecd24dde51853fdc
ryan <ryanlimjunrong@gmail.com>
2023-11-19 14:08:32 +0800

    updated report generation functionality to student menu (UNTESTED)

--- src/control/CampSystem.java
+++ src/control/CampSystem.java
@@ -8,6 +8,7 @@ import entity.Student;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 
 import entity.Camp;
 import entity.CampInformation;
@@ -94,4 +95,12 @@ public class CampSystem {
         return -1;
     }
 
+    public Camp getCampByName(String campName) {
+        for (Camp camp : camps) {
+            if (camp.getCampInfo().getCampName().equalsIgnoreCase(campName)) {
+                return camp;
+            }
+        }
+        return null;
+    }
 }
