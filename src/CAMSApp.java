d7a4101f748401d0c75a322b2b717fb3739ae64a
tersimdev <tersimdev@gmail.com>
2023-11-20 01:17:32 +0800

    added cleanup call if console app terminates suddenly

--- src/CAMSApp.java
+++ src/CAMSApp.java
@@ -8,7 +8,7 @@ import control.ReportSystem;
 /**
  * <p>
  * Entry point for our application.
- * Creates UI and System objects, 
+ * Creates UI and System objects,
  * with exception of DataStoreSystem which is a singleton
  * </p>
  * 
@@ -24,29 +24,42 @@ public class CAMSApp {
     private FeedbackSystem feedbackSystem;
     private ReportSystem reportSystem;
 
+    private boolean running = false;
+
     public void init() {
-        //create systems
+        // create systems
         dataStoreSystem = new DataStoreSystem();
         loginSystem = new LoginSystem(dataStoreSystem);
         campSystem = new CampSystem(dataStoreSystem);
         feedbackSystem = new FeedbackSystem(dataStoreSystem);
         reportSystem = new ReportSystem();
 
-
-        //create ui
+        // create ui
         consoleUI = new ConsoleUI();
         consoleUI.init(loginSystem, campSystem, feedbackSystem, reportSystem);
+
+        running = true;
     }
+
     /**
      * run app main update loop
+     * 
      * @return if app should exit
      */
     public boolean run() {
         return consoleUI.run();
     }
+
     public void cleanup() {
+        if (!running)
+            return; // alr cleaned
+        // cleanup stuff here
         consoleUI.cleanup();
-        //cleanup systems here
-        dataStoreSystem.cleanup();
+        dataStoreSystem.cleanup(); // crucial, as it saves data
+        running = false;
+    }
+
+    public boolean isRunning() {
+        return false;
     }
 };
--- src/Main.java
+++ src/Main.java
@@ -14,9 +14,20 @@ public class Main {
     public static void main(String[] args) {
         Log.enableLogging(true); // enable this for dev work
 
+        
         //create the app and run it
         CAMSApp app = new CAMSApp();
         app.init();
+
+        //create a runtimem hook to trigger cleanup on crash
+        Runtime.getRuntime().addShutdownHook( new Thread() { 
+            @Override 
+            public void run() { 
+                if (app.isRunning())
+                    app.cleanup();
+            }
+        });
+        
         boolean shouldExit = false;
         while(!shouldExit) {
            shouldExit = app.run();
